1e9fa9bd7fbbb4b9c34d4d8a008cc80ba290d344
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-04-18 15:54:48 -0700

    able to GET grades and display

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import Header from './header';
+import GradeTable from './grade-table';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+
+  }
+
+  componentDidMount() {
+    this.getGrades();
+  }
+
+  getGrades() {
+    fetch('/api/grades')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ grades: data });
+      })
+      .catch(error => console.log('Fetch failed!', error));
+
+  }
+
   render() {
     return (
-      <Header avgScore='84' />
+      <div>
+        <Header avgScore='84' />
+        <div className="container">
+          <div className="row">
+            <GradeTable grades={this.state.grades}/>
+          </div>
+        </div>
+      </div>
     );
   }
 }
---
+++ client/components/grade.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Grade extends React.Component {
+
+  render() {
+    const grade = this.props.grade;
+    return (
+      <tr>
+        <td className='col-4'>{grade.name}</td>
+        <td className='col-4'>{grade.course}</td>
+        <td className='col-4'>{grade.grade}</td>
+
+      </tr>
+    );
+  }
+}
